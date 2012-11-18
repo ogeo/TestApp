2af12b3e731147b1ffc8a3f97fb3792ec50d859d
DavideRoss <davide.ross93@gmail.com>
2012-11-18 01:31:08 +0100

    first commit

---
+++ TestApp/NumericUpDown.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TestApp
+{
+    public partial class NumericUpDown : UserControl
+    {
+        private bool _remove;
+
+        public int MaxValue { get; set; }
+        public int MinValue { get; set; }
+
+        public int Value
+        {
+            get { return int.Parse(textBlock2.Text); }
+            set { textBlock2.Text = value.ToString(); }
+        }
+
+        public string LabelString
+        {
+            get { return textBlock1.Text; }
+            set { textBlock1.Text = value; }
+        }
+
+        public NumericUpDown()
+        {
+            InitializeComponent();
+        }
+
+        private bool CheckLimit(int val)
+        {
+            return !(val > MaxValue || val < MinValue);
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckLimit(int.Parse(textBlock2.Text) - 1)) textBlock2.Text = (int.Parse(textBlock2.Text) - 1).ToString();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckLimit(int.Parse(textBlock2.Text) + 1)) textBlock2.Text = (int.Parse(textBlock2.Text) + 1).ToString();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            textBlock2.Text = ((MaxValue + MinValue) / 2).ToString();
+        }
+    }
+}
---
+++ TestApp/PhotoShoot.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Windows;
+using Microsoft.Devices;
+using Microsoft.Phone.Controls;
+using Microsoft.Xna.Framework.Media;
+
+namespace TestApp
+{
+    public partial class PhotoShoot : PhoneApplicationPage
+    {
+        private int _savedCounter;
+        private PhotoCamera _cam;
+        private readonly MediaLibrary _library = new MediaLibrary();
+
+        public PhotoShoot()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (Camera.IsCameraTypeSupported(CameraType.Primary))
+            {
+                _cam = new PhotoCamera(CameraType.Primary);
+
+                _cam.CaptureCompleted += CamCaptureCompleted;
+                _cam.CaptureImageAvailable += CamCaptureImageAvailable;
+                _cam.CaptureThumbnailAvailable += CamCaptureThumbnailAvailable;
+
+                viewfinderBrush.SetSource(_cam);
+            }
+        }
+
+        protected override void OnNavigatingFrom(System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            if (_cam == null) return;
+            _cam.Dispose();
+
+            _cam.CaptureCompleted -= CamCaptureCompleted;
+            _cam.CaptureImageAvailable -= CamCaptureImageAvailable;
+            _cam.CaptureThumbnailAvailable -= CamCaptureThumbnailAvailable;
+        }
+
+        private void CamCaptureCompleted(object sender, CameraOperationCompletedEventArgs e)
+        {
+            _savedCounter++;
+        }
+
+        private void CamCaptureImageAvailable(object sender, ContentReadyEventArgs e)
+        {
+            string fileName = _savedCounter + ".jpg";
+
+            try
+            {
+                _library.SavePictureToCameraRoll(fileName, e.ImageStream);
+                e.ImageStream.Seek(0, SeekOrigin.Begin);
+
+                using (IsolatedStorageFile isStore = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    using (
+                        IsolatedStorageFileStream targetStream = isStore.OpenFile(fileName, FileMode.Create,
+                                                                                  FileAccess.Write))
+                    {
+
+                        byte[] readBuffer = new byte[4096];
+                        int bytesRead;
+
+                        while ((bytesRead = e.ImageStream.Read(readBuffer, 0, readBuffer.Length)) > 0)
+                        {
+                            targetStream.Write(readBuffer, 0, bytesRead);
+                        }
+                    }
+                }
+            }
+            finally
+            {
+
+                e.ImageStream.Close();
+            }
+
+        }
+
+        public void CamCaptureThumbnailAvailable(object sender, ContentReadyEventArgs e)
+        {
+            string fileName = _savedCounter + "_th.jpg";
+
+            try
+            {
+                using (IsolatedStorageFile isStore = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    using (
+                        IsolatedStorageFileStream targetStream = isStore.OpenFile(fileName, FileMode.Create,
+                                                                                  FileAccess.Write))
+                    {
+                        byte[] readBuffer = new byte[4096];
+                        int bytesRead;
+
+                        while ((bytesRead = e.ImageStream.Read(readBuffer, 0, readBuffer.Length)) > 0)
+                        {
+                            targetStream.Write(readBuffer, 0, bytesRead);
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                e.ImageStream.Close();
+            }
+        }
+
+        private void ShutterButtonClick1(object sender, RoutedEventArgs e)
+        {
+            if (_cam == null) return;
+            _cam.CaptureImage();
+        }
+    }
+}
